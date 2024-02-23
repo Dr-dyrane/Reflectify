f4370470f3a29588b9820f752a16db86e7bed2d5
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2024-02-23 08:06:38 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-	<div className="flex text-eerie justify-center items-center min-h-screen">Home</div>
+	<div className="flex text-eerie dark:text-warm justify-center items-center min-h-screen text-2xl">Welcome to Reflectify: A safe space</div>
   )
 }
 
